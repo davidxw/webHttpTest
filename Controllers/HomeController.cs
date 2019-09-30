a702c532d13b8c96443feac9de9205cc9a725c7e
David Watson <dwatson@microsoft.com>
2019-09-30 21:38:36 +1000

    Working but not tested

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,28 +2,170 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
+using Microsoft.EntityFrameworkCore.Query.Expressions;
+using Newtonsoft.Json;
 using webHttpTest.Models;
 
 namespace webHttpTest.Controllers
 {
+
+
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
-            return View();
+            var model = new HttpRequestViewModel();
+
+            model.Method = "GET";
+            model.RequestContentType = "application/json";
+
+            return View(model);
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> SendRequest(HttpRequestViewModel viewModel)
         {
-            return View();
+            try
+            {
+                if (!string.IsNullOrEmpty(viewModel.Url))
+                {
+                    var httpRequest = new HttpRequestMessage();
+
+                    httpRequest.RequestUri = new Uri(viewModel.Url);
+
+                    // request headers
+                    if (!string.IsNullOrEmpty(viewModel.RequestHeaders))
+                    {
+                        var headerList = viewModel.RequestHeaders.Split(Environment.NewLine).ToList<string>();
+
+                        foreach (var headerString in headerList)
+                        {
+                            var headerArray = headerString.Split(":");
+
+                            if (headerArray.Length == 2)
+                            {
+                                httpRequest.Headers.Add(headerArray[0].Trim(), headerArray[1].Trim());
+                            }
+                        }
+                    }
+
+                    // method
+                    switch (viewModel.Method)
+                    {
+                        case "GET":
+                            httpRequest.Method = HttpMethod.Get;
+                            break;
+                        case "POST":
+                            httpRequest.Method = HttpMethod.Post;
+                            break;
+                        case "PUT":
+                            httpRequest.Method = HttpMethod.Put;
+                            break;
+                        case "PATCH":
+                            httpRequest.Method = HttpMethod.Patch;
+                            break;
+                        case "DELETE":
+                            httpRequest.Method = HttpMethod.Delete;
+                            break;
+                        default:
+                            throw new Exception($"Unknown HTTP Method - {viewModel.Method}");
+                    }
+
+                    // body
+                    if (!string.IsNullOrEmpty(viewModel.RequestBody))
+                    {
+                        httpRequest.Content = new StringContent(viewModel.RequestBody,
+                                        Encoding.UTF8,
+                                        viewModel.RequestContentType);
+                    }
+
+                    var httpClient = new HttpClient();
+
+                    var stopwatch = new Stopwatch();
+
+                    stopwatch.Start();
+
+                    var result = await httpClient.SendAsync(httpRequest);
+                    viewModel.ResponseBody = await result.Content.ReadAsStringAsync();
+
+                    stopwatch.Stop();
+
+                    viewModel.ResponseCode = (int)result.StatusCode;
+                    viewModel.ResponseCodeText = result.StatusCode.ToString();
+                    viewModel.ResponseBodyLength = result.Content.Headers.ContentLength.Value;
+                    viewModel.ResponseContentType = result.Content.Headers.ContentType.ToString();
+                    viewModel.ResponseTimeMilliseconds = stopwatch.ElapsedMilliseconds;
+
+                    //if (viewModel.ResponseContentType.ToLower().Contains("json"))
+                    //{
+                    //    viewModel.ResponseBody = FormatJson(viewModel.ResponseBody);
+                    //}
+
+                    viewModel.ResponseBody = PrettyPrint(viewModel.ResponseBody);
+
+
+                   viewModel.ResponseBody = Regex.Replace(viewModel.ResponseBody, $"({Environment.NewLine})+", $"{Environment.NewLine}")
+                        .Trim();
+
+                    // Headers 
+
+                    viewModel.ResponseHeaders = new List<Header>();
+
+                    foreach (var header in result.Headers)
+                    {
+                        viewModel.ResponseHeaders.Add(new Header { Key = header.Key, Value = header.Value.First() });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                viewModel.ErrorText = ex.Message;
+            }
+
+            return View("Index", viewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private string FormatJson(string json)
+        {
+            dynamic parsedJson = JsonConvert.DeserializeObject(json);
+            return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+        }
+
+        public string PrettyPrint(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            try
+            {
+                return XDocument.Parse(input).ToString();
+            }
+            catch (Exception) { }
+
+            try
+            {
+                var t = JsonConvert.DeserializeObject<object>(input);
+                return JsonConvert.SerializeObject(t, Formatting.Indented);
+            }
+            catch (Exception) { }
+
+            return input;
         }
     }
 }
---
+++ Models/HttpRequestViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace webHttpTest.Models
+{
+    public class HttpRequestViewModel
+    {
+        [Required]
+        public string Method { get; set; }
+
+        [Required]
+        [Url]
+        public string Url { get; set; }
+
+        public string RequestContentType { get; set; }
+
+        public string RequestHeaders { get; set; }
+
+        public string RequestBody { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        public string ResponseCodeText { get; set; }
+
+        public long ResponseTimeMilliseconds { get; set; }
+
+        public string ResponseBody { get; set; }
+
+        public long ResponseBodyLength { get; set; }
+
+        public string ResponseContentType { get; set; }
+
+        public List<Header> ResponseHeaders { get; set; }
+
+        public string ErrorText { get; set; }
+
+        public List<SelectListItem> Methods { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "GET", Text="GET" },
+            new SelectListItem { Value = "POST", Text ="POST" },
+            new SelectListItem { Value = "PUT", Text ="PUT" },
+            new SelectListItem { Value = "PATCH", Text ="PATCH" },
+            new SelectListItem { Value = "DELETE", Text ="DELETE" }
+        };
+
+        public List<SelectListItem> ContentTypes { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "application/json", Text="JSON (application/json)" },
+            new SelectListItem { Value = "text/html", Text="HTML (text/html)" },
+            new SelectListItem { Value = "application/xml", Text="XML (application/xml)" },
+            new SelectListItem { Value = "text/plain", Text="TEXT (text/plain)" },
+            new SelectListItem { Value = "application/x-www.form-urlencoded", Text="FORM URL Encoded (application/x-www.form-urlencoded)" }
+        };
+    }
+
+    public class Header
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
