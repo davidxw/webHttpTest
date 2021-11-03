4083bf1521ce8b32aa8df483f202d8c1f5424b11
David Watson <dwatson@microsoft.com>
2021-11-03 16:18:02 +1100

    .NET6 upgrade

--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.ProjectModel;
 using webHttpTest.Hubs;
 using webHttpTest.Services;
 
@@ -35,15 +34,15 @@ namespace webHttpTest
             });
 
             services.AddSignalR();
-            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
 
             services.AddScoped<INetworkService, NetworkService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.EnvironmentName.Equals("Development"))
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -71,6 +70,8 @@ namespace webHttpTest
                 endpoints.MapRazorPages();
                 endpoints.MapHub<TraceRtHub>("/traceRtHub");
             });
+
+      
         }
     }
 }
